7d27b20b8e020603bd50d553a66541c91c7ca6de
Sohyeon <soso981227@naver.com>
2022-06-30 23:42:12 +0900

    ver1(폼 제출 및 유저정보 저장)

---
+++ app.js
@@ -0,0 +1,21 @@
+const loginForm = document.querySelector("#login");
+const loginInput = document.querySelector("#login input");
+const welcome = document.querySelector("#welcome");
+
+function FormSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add("hidden");
+  const username = loginInput.value;
+  localStorage.setItem("username", username);
+  welcome.innerText = `안녕하세요, ${username}님!`;
+  welcome.classList.remove("hidden");
+}
+
+const SavedName = localStorage.getItem("username");
+if (SavedName === null) {
+  loginForm.classList.remove("hidden");
+  loginForm.addEventListener("submit", FormSubmit);
+} else {
+  welcome.innerText = `안녕하세요, ${username}님!`;
+  welcome.classList.remove("hidden");
+}
